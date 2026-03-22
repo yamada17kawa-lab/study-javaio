ec08d068ca614fa90d03af4d0d1136ef4f6f730d
YangWangWang <yamada17kawa@gmail.com>
2026-03-22 23:37:59 +0800

    初始化仓库

---
+++ src/main/java/com/nuliyang/study_javaio/StudyJavaioApplication.java
@@ -0,0 +1,13 @@
+package com.nuliyang.study_javaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJavaioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJavaioApplication.class, args);
+    }
+
+}
